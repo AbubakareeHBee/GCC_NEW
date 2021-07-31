432a1038890abf3624e4ad8c12701c9427c678e2
abubakarhb <assadeeq543@gmail.com>
2021-07-31 11:35:21 +0100

    connection to local DB and online DB have been define

--- php/Connections/gcc.php
+++ php/Connections/gcc.php
@@ -1,7 +1,19 @@
 <?php
+
+#Online server connection
+#comment while on local
 $hostname_gcc = "localhost";
 $database_gcc = "steazhex_gcc";
 $username_gcc = "steazhex_gcc";
 $password_gcc = "14)(Ya1y5*AT";
 $gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysql_error(),E_USER_ERROR); 
+
+#local server connection
+#comment the below code before pushing to git
+// $hostname_gcc = "localhost";
+// $database_gcc = "gcc";
+// $username_gcc = "root";
+// $password_gcc = "";
+// $gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysql_error(),E_USER_ERROR); 
+
 ?>
