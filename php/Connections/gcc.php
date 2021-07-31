b27c42ca7a14e76a9b3457d9a34fd61a24d8ed85
aliyukamilu <aliyukamilu@steamledge.com>
2021-07-31 19:53:20 +0100

    update

--- php/Connections/gcc.php
+++ php/Connections/gcc.php
@@ -1,7 +1,18 @@
+<!-- ONLINE  -->
+
 <?php
 $hostname_gcc = "localhost";
 $database_gcc = "steazhex_gcc";
 $username_gcc = "steazhex_gcc";
 $password_gcc = "14)(Ya1y5*AT";
-$gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysql_error(),E_USER_ERROR); 
+$gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysqli_error($gcc),E_USER_ERROR); 
+
+
+
+#loca server connection
+// $hostname_gcc = "localhost";
+// $database_gcc = "gcc";
+// $username_gcc = "root";
+// $password_gcc = "";
+// $gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysqli_error($gcc),E_USER_ERROR); 
 ?>
