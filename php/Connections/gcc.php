31d73f2ac1e8d4fca82d9515ccd00361e06b7545
aliyukamilu <aliyukamilu@steamledge.com>
2021-07-31 16:01:13 +0100

    login update

--- php/Connections/gcc.php
+++ php/Connections/gcc.php
@@ -2,18 +2,18 @@
 
 #Online server connection
 #comment while on local
-$hostname_gcc = "localhost";
-$database_gcc = "steazhex_gcc";
-$username_gcc = "steazhex_gcc";
-$password_gcc = "14)(Ya1y5*AT";
-$gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysql_error(),E_USER_ERROR); 
+// $hostname_gcc = "localhost";
+// $database_gcc = "steazhex_gcc";
+// $username_gcc = "steazhex_gcc";
+// $password_gcc = "14)(Ya1y5*AT";
+// $gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysqli_error($gcc),E_USER_ERROR); 
 
 #local server connection
 #comment the below code before pushing to git
-// $hostname_gcc = "localhost";
-// $database_gcc = "gcc";
-// $username_gcc = "root";
-// $password_gcc = "";
-// $gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysql_error(),E_USER_ERROR); 
+$hostname_gcc = "localhost";
+$database_gcc = "gcc";
+$username_gcc = "root";
+$password_gcc = "";
+$gcc = mysqli_connect($hostname_gcc, $username_gcc, $password_gcc, $database_gcc) or trigger_error(mysqli_error($gcc),E_USER_ERROR); 
 
 ?>
